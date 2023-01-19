121d1163c3fb6cb67b8f8772071651c8787aee71
Eddy <edersenc@gmail.com>
2023-01-19 18:05:56 -0500

    x

--- startup.py
+++ startup.py
@@ -4,8 +4,18 @@ import subprocess
 bluetooth_status = subprocess.run(['systemctl', 'is-active', 'bluetooth'], stdout=subprocess.PIPE)
 
 if bluetooth_status.stdout.decode().strip() == 'active':
+    # Open the bluetooth command-line interface
+    process = subprocess.Popen(['bluetoothctl'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-    subprocess.run(['bluetoothctl','connect 88:64:40:2B:7B:41','exit'])
+    # Send the 'connect XX:XXX:XX:XX' command to the bluetooth command-line interface
+    process.stdin.write(b'connect 88:64:40:2B:7B:41\n')
+    process.stdin.flush()
+    # Send the 'exit' command to the bluetooth command-line interface
+    process.stdin.write(b'exit\n')
+    process.stdin.flush()
+
+    # Wait for the process to end
+    process.wait()
 
     scripts = ['update.py', 'camerahandler.py']
     process = None
@@ -13,7 +23,6 @@ if bluetooth_status.stdout.decode().strip() == 'active':
     for script in scripts:
         process = subprocess.Popen(['python', script], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, close_fds=True)
         process.wait()
-
 
 
 
