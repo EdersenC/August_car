a04fb6a4f2f6e00cd7fca20206ff951eb1662276
Eddy <edersenc@gmail.com>
2023-01-19 23:08:17 -0500

    d

--- camerahandler.py
+++ camerahandler.py
@@ -2,6 +2,29 @@ import datetime
 import os
 import subprocess
 import time
+
+def startBluetooth():
+    with open("/home/august/config.txt", "r") as f:
+    # Read the save directory from the first line of the file
+        save_directory = f.readline().strip()
+    # Read the bluetooth MAC address from the second line of the file
+        bluetooth_mac = f.readline().strip()
+
+# Check if bluetooth service is running
+    bluetooth_status = subprocess.run(['systemctl', 'is-active', 'bluetooth'], stdout=subprocess.PIPE)
+
+    if bluetooth_status.stdout.decode().strip() == 'active':
+    # Open the bluetooth command-line interface
+        process = subprocess.Popen(['bluetoothctl'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    # Send the 'connect XX:XXX:XX:XX' command to the bluetooth command-line interface
+        process.stdin.write(f'connect {bluetooth_mac}\n'.encode())
+        process.stdin.flush()
+    # Send the 'exit' command to the bluetooth command-line interface
+        process.stdin.write(b'exit\n')
+        process.stdin.flush()
+
+
 def startCamera(filename):
     # Define the directory where the video will be saved
     now = datetime.datetime.now()
@@ -40,6 +63,7 @@ while(True):
     now = datetime.datetime.now()
     file_name = now.strftime("%Y-%m-%d %I-%M %p") + '.avi'
     command = startCamera(file_name)
+    command2 = startBluetooth()
     command.wait()
     command.terminate()
 
