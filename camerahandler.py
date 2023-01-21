7d3bee2c61c5fea20da5b9430320949ed8ad19e7
Eddy <edersenc@gmail.com>
2023-01-21 18:02:34 -0500

    f

--- camerahandler.py
+++ camerahandler.py
@@ -11,7 +11,22 @@ with open("/home/august/carsetup/config.json", "r") as f:
     bluetooth_mac = config["bluetooth_mac"]
     hotspot_ssid = config["hotspot_ssid"] 
     hotspot_password = config["hotspot_password"]
+# Use the settings in your code
+def startBluetooth():
+    # Check if bluetooth service is running
+    bluetooth_status = subprocess.run(['systemctl', 'is-active', 'bluetooth'], stdout=subprocess.PIPE)
 
+    if bluetooth_status.stdout.decode().strip() == 'active':
+        # Open the bluetooth command-line interface
+        process = subprocess.Popen(['bluetoothctl'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        # Send the 'connect XX:XXX:XX:XX' command to the bluetooth command-line interface
+        process.stdin.write(f'connect {bluetooth_mac}\n'.encode())
+        process.stdin.flush()
+        # Send the 'exit' command to the bluetooth command-line interface
+        process.stdin.write(b'exit\n')
+        process.stdin.flush()
+        
 def update():
     # Get current date
     now = datetime.datetime.now()
@@ -50,21 +65,7 @@ def update():
     # Exit the script
     exit(result.returncode)
 
-# Use the settings in your code
-def startBluetooth():
-    # Check if bluetooth service is running
-    bluetooth_status = subprocess.run(['systemctl', 'is-active', 'bluetooth'], stdout=subprocess.PIPE)
 
-    if bluetooth_status.stdout.decode().strip() == 'active':
-        # Open the bluetooth command-line interface
-        process = subprocess.Popen(['bluetoothctl'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-        # Send the 'connect XX:XXX:XX:XX' command to the bluetooth command-line interface
-        process.stdin.write(f'connect {bluetooth_mac}\n'.encode())
-        process.stdin.flush()
-        # Send the 'exit' command to the bluetooth command-line interface
-        process.stdin.write(b'exit\n')
-        process.stdin.flush()
         
 
 def startCamera(filename):
