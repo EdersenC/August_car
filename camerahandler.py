c2dcbd326537ae6a653d9ed2e7684b586d52bc5b
Eddy <edersenc@gmail.com>
2023-01-21 06:25:19 -0500

    h

--- camerahandler.py
+++ camerahandler.py
@@ -27,7 +27,44 @@ def startBluetooth():
         # Send the 'exit' command to the bluetooth command-line interface
         process.stdin.write(b'exit\n')
         process.stdin.flush()
-        subprocess.run(["python3", "update.py"])
+        
+def update():
+    # Get current date
+    now = datetime.datetime.now()
+    version_date = now.strftime("%Y-%m-%d")
+
+
+    # Run the git pull command with the username and password as command line arguments
+    result = subprocess.run(["git", "pull"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    # Print the output of the command
+    print(result.stdout.decode())
+    print(result.stderr.decode())
+
+    # Check the return code
+    if result.returncode == 0:
+        # Get current version from git tag
+        version = subprocess.run(["git", "describe", "--tags"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        current_version = version.stdout.decode().strip()
+        # Append version date to current version
+        new_version = f"{version_date}-{current_version}"
+        print("New version: ", new_version)
+        # Write new version to text files
+        with open("/home/august/carsetup/August_car/AugustCar_SoftWareVersion.txt", "w") as f:
+            f.write(new_version)
+        print("Command completed successfully.")
+        subprocess.run(["git", "pull"])
+        subprocess.run(["sudo", "apt-get", "update", ""])
+        subprocess.run(["sudo", "apt-get", "upgrade", "-y", "bluetooth", "--allow-unauthenticated"])
+        subprocess.run(["sudo", "apt-get", "upgrade", "-y", "wifi", "--allow-unauthenticated"])
+        subprocess.run(["sudo", "apt-get", "install", "-y", "fswebcam", "--allow-unauthenticated"])
+
+        print("Bluetooth, WiFi and fswebcam updated successfully.")
+    else:
+        print("Command failed with return code", result.returncode)
+
+    # Exit the script
+    exit(result.returncode)
 
 def startCamera(filename):
     # Define the directory where the video will be saved
@@ -56,8 +93,9 @@ def startCamera(filename):
     process = subprocess.Popen(command)
     return process
 command2 = startBluetooth()
+command3 = update()
 while(True):
-    
+
     now = datetime.datetime.now()
     file_name = now.strftime("%Y-%m-%d %I-%M %p") + '.avi'
     command = startCamera(file_name)
